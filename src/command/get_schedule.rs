dd9b2cc9253fece4a50eddcb140091ce16e09a2c
azazak123 <azazaka2002@gmail.com>
2023-05-18 20:33:38 +0300

    feat: add printing table for generated schedule

--- src/command/get_schedule.rs
+++ src/command/get_schedule.rs
@@ -1,7 +1,12 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, iter};
 
 use crate::parser::GraphParser;
 use color_eyre::{eyre::Context, Result};
+use tabled::{
+    builder::Builder,
+    settings::{Alignment, Style},
+    Table,
+};
 
 use super::Algorithm;
 
@@ -22,7 +27,30 @@ where
         Algorithm::Gc => graph.gc_schedule(profile),
     };
 
-    println!("Schedule using algorithm {}: {:?}", algorithm, schedule);
+    let table = create_schedule_table(schedule);
+
+    println!("Schedule using algorithm {}:\n{}", algorithm, table);
 
     Ok(())
+}
+
+fn create_schedule_table(schedule: Vec<Vec<Option<u128>>>) -> Table {
+    let mut builder = Builder::default();
+
+    for slot in schedule {
+        let slot_string: Vec<String> = iter::once(&Some(slot.len() as u128))
+            .chain(slot.iter())
+            .map(|v| match v {
+                Some(j) => j.to_string(),
+                None => "-".to_string(),
+            })
+            .collect();
+
+        builder.push_record(slot_string);
+    }
+
+    let mut table = builder.index().column(0).name(None).transpose().build();
+    table.with(Style::modern()).with(Alignment::center());
+
+    table
 }
